f53b0cd80ee04e12819d1edf1f7c1529aa37d1cd
Daniel Krupiński <danielkrupinski@outlook.com>
2019-03-14 20:37:39 +0100

    Update offsets

--- Feb.asm
+++ Feb.asm
@@ -123,9 +123,9 @@ localPlayerFlags dd ?
 
 section '.rdata' data readable
 
-localPlayerOffset dd 0xCCB774
+localPlayerOffset dd 0xCD2764
 flagsOffset dd 0x104
-forceJumpOffset dd 0x517F624
+forceJumpOffset dd 0x5186998
 forceJump dd 6
 sleepDuration dq -1
 
